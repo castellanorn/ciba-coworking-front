bcebf812bfcbb6435bf5dc15a6e84c20d201d1ff
castellanorn <castellanorn@gmail.com>
2024-09-18 14:23:10 +0200

    review frontend init

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router";
-import { AuthProvider } from "./context/auth/authContext";
+import { AuthProvider } from "./auth/AuthWrapper";
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,16 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
-import PrivateRoute from "./PrivateRoutes";
+import PrivateRoute from "./PrivateRoute";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
-import SignIn from "../pages/SignIn";
-import BranchPage from "../pages/BranchPage";
-import TopicPage from "../pages/TopicPage";
-import CreateTopicPage from "../pages/CreateTopicPage";
-import CreateCommentPage from "../pages/CreateCommentPage";
-import EditCommentPage from "../pages/EditCommentPage";
-import EditProfilePage from "../pages/EditProfilePage";
-
-export const router = createBrowserRouter ([
+/* import SignIn from "../pages/SignIn"; */
+/* import BranchPage from "../pages/BranchPage"; */
+/* import TopicPage from "../pages/TopicPage"; */
+/* import CreateTopicPage from "../pages/CreateTopicPage"; */
+/* import CreateCommentPage from "../pages/CreateCommentPage"; */
+/* import EditCommentPage from "../pages/EditCommentPage"; */
+/* import EditProfilePage from "../pages/EditProfilePage"; */
+/*  */
+const router = createBrowserRouter ([
     {
         path: "/",
         element: <Home />,
@@ -19,45 +19,47 @@ export const router = createBrowserRouter ([
         path: "login",
         element: <Login />
     },
-    {
-        path: "signin",
-        element: <SignIn />
-    },
-    {
-        path: "branch/:id",
-        element: <BranchPage />
-    },
-    {
-        path: "topic/:id",
-        element: <TopicPage />
-    },
+ /*    { */
+ /*        path: "signin", */
+ /*        element: <SignIn /> */
+ /*    }, */
+ /*    { */
+ /*        path: "branch/:id", */
+ /*        element: <BranchPage /> */
+ /*    }, */
+ /*    { */
+ /*        path: "topic/:id", */
+ /*        element: <TopicPage /> */
+ /*    }, */
     {
         path: "create_topic",
         element: (
             <PrivateRoute>
-                <CreateTopicPage />
+                {/* <CreateTopicPage /> */}
             </PrivateRoute>)
     },
     {
         path: "create_comment",
         element: (
             <PrivateRoute>
-                <CreateCommentPage />
+                {/* <CreateCommentPage /> */}
             </PrivateRoute>)
     },
     {
         path: "edit_comment/:id",
         element: (
             <PrivateRoute>
-                <EditCommentPage />
+                {/* <EditCommentPage /> */}
             </PrivateRoute>)
     },
     {
         path: "edit_profile/:id",
         element: (
             <PrivateRoute>
-                <EditProfilePage />
+               {/*  <EditProfilePage /> */}
             </PrivateRoute>
             )
     }
 ])
+
+export default router;
