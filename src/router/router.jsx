dfe4d0df9265eefe7c7f51b2a8c7ccb1831244b4
castellanorn <castellanorn@gmail.com>
2024-09-19 11:12:24 +0200

    config front, icons, styled, color, font

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,64 +2,53 @@ import { createBrowserRouter } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import Layout from "../layout/Layout";
 /* import SignIn from "../pages/SignIn"; */
 /* import BranchPage from "../pages/BranchPage"; */
 /* import TopicPage from "../pages/TopicPage"; */
 /* import CreateTopicPage from "../pages/CreateTopicPage"; */
-/* import CreateCommentPage from "../pages/CreateCommentPage"; */
-/* import EditCommentPage from "../pages/EditCommentPage"; */
 /* import EditProfilePage from "../pages/EditProfilePage"; */
 /*  */
-const router = createBrowserRouter ([
+const router = createBrowserRouter([
     {
         path: "/",
-        element: <Home />,
-    },
-    {
-        path: "login",
-        element: <Login />
-    },
- /*    { */
- /*        path: "signin", */
- /*        element: <SignIn /> */
- /*    }, */
- /*    { */
- /*        path: "branch/:id", */
- /*        element: <BranchPage /> */
- /*    }, */
- /*    { */
- /*        path: "topic/:id", */
- /*        element: <TopicPage /> */
- /*    }, */
-    {
-        path: "create_topic",
-        element: (
-            <PrivateRoute>
-                {/* <CreateTopicPage /> */}
-            </PrivateRoute>)
-    },
-    {
-        path: "create_comment",
-        element: (
-            <PrivateRoute>
-                {/* <CreateCommentPage /> */}
-            </PrivateRoute>)
-    },
-    {
-        path: "edit_comment/:id",
-        element: (
-            <PrivateRoute>
-                {/* <EditCommentPage /> */}
-            </PrivateRoute>)
-    },
-    {
-        path: "edit_profile/:id",
-        element: (
-            <PrivateRoute>
-               {/*  <EditProfilePage /> */}
-            </PrivateRoute>
-            )
-    }
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+            {
+                path: "login",
+                element: <Login />,
+            },
+            /*    { */
+            /*        path: "signin", */
+            /*        element: <SignIn /> */
+            /*    }, */
+            /*    { */
+            /*        path: "branch/:id", */
+            /*        element: <BranchPage /> */
+            /*    }, */
+            /*    { */
+            /*        path: "topic/:id", */
+            /*        element: <TopicPage /> */
+            /*    }, */
+            {
+                path: "create_topic",
+                element: (
+                    <PrivateRoute>
+                        {/* <CreateTopicPage /> */}
+                    </PrivateRoute>)
+            },
+            {
+                path: "edit_profile/:id",
+                element: (
+                    <PrivateRoute>
+                        {/*  <EditProfilePage /> */}
+                    </PrivateRoute>
+                )
+            }]
+},
 ])
-
 export default router;
