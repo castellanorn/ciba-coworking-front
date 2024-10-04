242c0d9366f8ef5e172acece8f7a92bb0da2346a
Mercedes-Celedon <mercedes.celedon.95@gmail.com>
2024-10-04 01:06:25 -0600

    adding api and endpoints

--- src/pages/admin/ManageMeetingRoom.jsx
+++ src/pages/admin/ManageMeetingRoom.jsx
@@ -1,7 +1,29 @@
+import { Line } from '../../components/title/TitleStyled'
+import ContainerButtons from '../../components/container/ButtonsContainer'
+import PlacesButton from "../../components/buttons/PlacesButton"
+import TitleMobile from '../../components/title/Title'
 
 const ManageMeetingRoom = () => {
   return (
     <div>
+      <TitleMobile title="Edició de reserves" />
+      <ContainerButtons>
+            <PlacesButton
+                text="Taules individuals"
+                link="/gestio-de-taules"
+                focus={false}
+            />
+            <PlacesButton
+                text="Oficines privades"
+                link="/gestio-oficina"
+                focus={false}
+            />
+            <PlacesButton
+                text="Sala de reunions"
+                focus={true}
+            />
+        </ContainerButtons>
+        <Line />
       
     </div>
   )
