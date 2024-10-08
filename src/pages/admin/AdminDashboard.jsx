b00c06618dec15ea57adb8d10261b6a6b050c64a
Mercedes-Celedon <mercedes.celedon.95@gmail.com>
2024-10-08 02:59:05 -0600

    hotfix table issues

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import Table from "../../components/table/Table"
 import TableMobile from "../../components/table/TableMobile"
 import TitleMobile from "../../components/title/Title";
